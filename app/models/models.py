5a9a3b48f61fc8c1f507b2f17b87f5312dfcde53
Nelson Carrasquel <carrasquel@outlook.com>
2020-03-24 13:52:08 -0300

    Basic model for user authentication

--- app/models/models.py
+++ app/models/models.py
@@ -1,2 +1,42 @@
 # -*- coding: utf-8 -*-
 # app/models/models.py
+
+"""models.py
+
+This module implements classes for
+modelling of the Property Management System API.
+"""
+
+import json
+from datetime import datetime, date
+
+from flask_restplus import fields
+from peewee import Model, SqliteDatabase, TextField, FloatField, DateField, ForeignKeyField
+
+db = SqliteDatabase("id.db")
+
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+
+class UserRole(BaseModel):
+
+    role = TextField()
+
+
+class User(BaseModel):
+
+    username = TextField()
+    password = TextField()
+    role = ForeignKeyField(UserRole)
+
+
+class Authentication(BaseModel):
+
+    user = ForeignKeyField(User)
+    key = TextField()
+
+    expire = DateField()
