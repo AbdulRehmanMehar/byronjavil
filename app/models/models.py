e5ad3cab48ac3e4d46a58707c5957f775c5149ab
Nelson Carrasquel <carrasquel@outlook.com>
2020-03-26 11:24:32 -0300

    Models updated

--- app/models/models.py
+++ app/models/models.py
@@ -11,7 +11,7 @@ import json
 from datetime import datetime, date
 
 from flask_restplus import fields
-from peewee import Model, SqliteDatabase, TextField, BlobField, FloatField, DateField, ForeignKeyField
+from peewee import Model, SqliteDatabase, TextField, BlobField, FloatField, DateField, DateTimeField, ForeignKeyField
 
 db = SqliteDatabase("id.db")
 
@@ -50,10 +50,16 @@ class Customer(BaseModel):
     
     user = ForeignKeyField(User)
 
+
 class Comment(BaseModel):
 
     text = TextField()
     user = ForeignKeyField(User)
+
+
+class Order(BaseModel):
+
+    type = TextField()
 
 
 class ResearchState(BaseModel):
@@ -63,6 +69,15 @@ class ResearchState(BaseModel):
 
 class Research(BaseModel):
 
+    address = TextField()
+    
+    date_assigned = DateField(default=date.today)
+    due_date = DateField(default=date.today)
+    
+    company = ForeignKeyField(Customer)
+    user = ForeignKeyField(User)
+    
+    order = ForeignKeyField(Order)
     state = ForeignKeyField(ResearchState)
     
 
@@ -75,6 +90,30 @@ class Attachment(BaseModel):
     base64 = BlobField()
 
 
+class ResearchComment(BaseModel):
+
+    research = ForeignKeyField(column_name='research_id', field='id', model=Research)
+    comment = ForeignKeyField(column_name='comment_id', field='id', model=Comment)
+
+
+class ResearchAttachment(BaseModel):
+
+    research = ForeignKeyField(column_name='research_id', field='id', model=Research)
+    attachment = ForeignKeyField(column_name='attachment_id', field='id', model=Attachment)
+
+
+class ResearchPicture(BaseModel):
+
+    research = ForeignKeyField(column_name='research_id', field='id', model=Research)
+    attachment = ForeignKeyField(column_name='attachment_id', field='id', model=Attachment)
+
+
+class EmailTemplate(BaseModel):
+
+    name = TextField()
+    template = TextField()
+
+
 def reset():
 
     db.connect()
@@ -83,13 +122,22 @@ def reset():
         UserRole,
         User,
         Authentication,
-        Attachment
+        Customer,
+        Comment,
+        Order,
+        ResearchState,
+        Research,
+        Attachment,
+        ResearchComment,
+        ResearchAttachment,
+        ResearchPicture,
+        EmailTemplate
     ]
 
     db.drop_tables(models)
     db.create_tables(models)
 
-    ROLES = ["SUPERVISOR", "RESEARCH", "DATA", "MANAGER"]
+    ROLES = ["SUPERVISOR", "RESEARCH", "DATA", "MANAGER", "SUPERVISOR/MANAGER"]
 
     for ROLE in ROLES:
 
