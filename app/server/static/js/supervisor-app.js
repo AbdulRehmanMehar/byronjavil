932a9222a32f1237174030bb32cbb6b0c7e495f0
Nelson Carrasquel <carrasquel@outlook.com>
2020-03-28 18:20:28 -0300

    Supervisor app composition

--- app/server/static/js/supervisor-app.js
+++ app/server/static/js/supervisor-app.js
@@ -1,5 +1,28 @@
 // supervisor-app.js
 
+var order_type_vm = new Vue({
+    el: '#order-type-app',
+
+    data: {
+        visible: true,
+    },
+
+    ready: function(){
+
+    },
+
+    methods: {
+
+        hide: function(){
+            this.visible = false;
+        },
+
+        show: function(){
+            this.visible = false;
+        }
+    }
+})
+
 var vm = new Vue({
 
     el: '#app',
@@ -19,6 +42,14 @@ var vm = new Vue({
     methods: {
 
         // Fetch methods
+        show: function(app){
+            console.log(app);
+            if (app == 'order-type'){
+                order_type_vm.toggleShow();
+            }
+
+        },
+
         fetchUsers: function(){
 
         },
