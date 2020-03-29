f2b3bf12e563d03f8f11805be95cbb6e9650fe31
Nelson Carrasquel <carrasquel@outlook.com>
2020-03-28 23:45:56 -0300

    Progress bar improved

--- app/server/static/js/dialog.js
+++ app/server/static/js/dialog.js
@@ -8,7 +8,7 @@ var waitingDialog = waitingDialog || (function ($) {
 		'<div class="modal-content">' +
 			'<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
 			'<div class="modal-body">' +
-				'<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+				'<div class="progress" style="margin-bottom:0;"><div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 100%"></div></div>' +
 			'</div>' +
 		'</div></div></div>');
 
@@ -36,10 +36,7 @@ var waitingDialog = waitingDialog || (function ($) {
 
 			// Configuring dialog
 			$dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
-			$dialog.find('.progress-bar').attr('class', 'progress-bar');
-			if (settings.progressType) {
-				$dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
-			}
+			
 			$dialog.find('h3').text(message);
 			// Adding callbacks
 			if (typeof settings.onHide === 'function') {
