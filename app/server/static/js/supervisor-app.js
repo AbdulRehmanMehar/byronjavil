10394bea77f3e51241137c131574ca850f58c650
Nelson Carrasquel <carrasquel@outlook.com>
2020-03-29 19:46:06 -0300

    Supervisor show hide methods

--- app/server/static/js/supervisor-app.js
+++ app/server/static/js/supervisor-app.js
@@ -19,7 +19,7 @@ var order_type_vm = new Vue({
         },
 
         show: function(){
-            this.visible = false;
+            this.visible = true;
         },
 
         setKey: function(key){
@@ -58,7 +58,7 @@ var users_vm = new Vue({
         },
 
         show: function(){
-            this.visible = false;
+            this.visible = true;
         },
 
         setKey: function(key){
@@ -71,6 +71,123 @@ var users_vm = new Vue({
             this.$http.get('api/supervisor/users', {headers: {'X-API-KEY': apiKey}})
                 .then(function (res){
                     this.users = res.data;
+                }, function(err){
+                    console.log(err);
+                })
+        }
+    }
+})
+
+var customers_vm = new Vue({
+    el: '#customers-app',
+
+    data: {
+        visible: true,
+        customers: [],
+        apiKey: null,
+    },
+
+    ready: function(){
+    },
+
+    methods: {
+
+        hide: function(){
+            this.visible = false;
+        },
+
+        show: function(){
+            this.visible = true;
+        },
+
+        setKey: function(key){
+            this.apiKey = key;
+        },
+
+        fetchCustomers: function(){
+            var apiKey = this.apiKey;
+
+            this.$http.get('api/supervisor/customer', {headers: {'X-API-KEY': apiKey}})
+                .then(function (res){
+                    this.customers = res.data;
+                }, function(err){
+                    console.log(err);
+                })
+        }
+    }
+})
+
+var orders_vm = new Vue({
+    el: '#orders-app',
+
+    data: {
+        visible: true,
+        orders: [],
+        apiKey: null,
+    },
+
+    ready: function(){
+    },
+
+    methods: {
+
+        hide: function(){
+            this.visible = false;
+        },
+
+        show: function(){
+            this.visible = true;
+        },
+
+        setKey: function(key){
+            this.apiKey = key;
+        },
+
+        fetchOrders: function(){
+            var apiKey = this.apiKey;
+
+            this.$http.get('api/supervisor/orders', {headers: {'X-API-KEY': apiKey}})
+                .then(function (res){
+                    this.orders = res.data;
+                }, function(err){
+                    console.log(err);
+                })
+        }
+    }
+})
+
+var attachments_vm = new Vue({
+    el: '#attachments-app',
+
+    data: {
+        visible: true,
+        attachments: [],
+        apiKey: null,
+    },
+
+    ready: function(){
+    },
+
+    methods: {
+
+        hide: function(){
+            this.visible = false;
+        },
+
+        show: function(){
+            this.visible = true;
+        },
+
+        setKey: function(key){
+            this.apiKey = key;
+        },
+
+        fetchAttachments: function(){
+            var apiKey = this.apiKey;
+
+            this.$http.get('api/supervisor/attachments', {headers: {'X-API-KEY': apiKey}})
+                .then(function (res){
+                    this.orders = res.data;
                 }, function(err){
                     console.log(err);
                 })
@@ -93,20 +210,49 @@ var vm = new Vue({
     ready: function(){
         order_type_vm.setKey(this.apiKey);
         users_vm.setKey(this.apiKey);
+        customers_vm.setKey(this.apiKey);
+        orders_vm.setKey(this.apiKey);
+        attachments_vm.setKey(this.apiKey);
         
         order_type_vm.fetchOrderTypes();
         users_vm.fetchUsers();
+        
+        this.show('order-type');
     },
 
     methods: {
 
-        // Fetch methods
         show: function(app){
-            console.log(app);
+            this.hide();
+            
             if (app == 'order-type'){
-                order_type_vm.toggleShow();
+                order_type_vm.show();
             }
 
+            if (app == 'users'){
+                users_vm.show();
+            }
+
+            if (app == 'customers'){
+                customers_vm.show();
+            }
+
+            if (app == 'orders'){
+                orders_vm.show();
+            }
+
+            if (app == 'attachments'){
+                attachments_vm.show();
+            }
+
+        },
+
+        hide: function(){
+            order_type_vm.hide();
+            users_vm.hide();
+            customers_vm.hide();
+            orders_vm.hide();
+            attachments_vm.hide();
         },
 
         fetchUsers: function(){
