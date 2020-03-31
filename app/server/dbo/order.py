bae6fad5f2e607a62c049afa97ed514170d3af48
Nelson Carrasquel <carrasquel@outlook.com>
2020-03-31 11:54:25 -0300

    Order dbo

--- app/server/dbo/order.py
+++ app/server/dbo/order.py
@@ -3,22 +3,32 @@
 
 from app.models import OrderType, OrderState, OrderAttachment
 from app.models import OrderComment, OrderPicture
-from app.models import Order
+from app.models import Order, Comment, Attachment
 
 
 class OrderDBO:
 
-    def create(self):
+    def create(self, _type, state, company, user, **kwargs):
 
         pass
 
     def read(self, _id):
 
-        pass
+        order = Order.select().where(Order.id==_id).get()
+
+        return order
 
     def read_all(self):
 
-        pass
+        result = list()
+
+        orders = Order.select()
+
+        for order in orders:
+
+            result.append(order)
+
+        return result
 
     def update(self, _id):
 
@@ -26,7 +36,9 @@ class OrderDBO:
 
     def delete(self, _id):
 
-        pass
+        order = self.read(_id)
+
+        order.delete_instance()
 
     def assign_user(self, user):
 
@@ -36,18 +48,51 @@ class OrderDBO:
 
         pass
 
-    def append_comment(self, comment):
+    def append_comment(self, _id, user, comment):
+        
+        order = self.read(_id)
 
-        pass
+        comment = Comment.create(user=user, text=comment)
+        OrderComment.create(order=order, comment=comment)
+
+        return True
 
     def get_comments(self, _id):
 
-        pass
+        result = list()
 
-    def append_attachment(self, attachment):
+        order = self.read(_id)
+        
+        order_comments = OrderComment.select().where(OrderComment.order_id==order.id).get()
 
-        pass
+        for order_comment in order_comments:
+
+            comment = Comment.select().where(Comment.id==order_comment.comment_id).get()
+
+            result.append(comment)
+
+        return result
+
+    def append_attachment(self, _id, attachment):
+
+        order = self.read(_id)
+
+        OrderAttachment.create(order=order, attachment=attachment)
+
+        return True
 
     def get_attachments(self, _id):
 
-        pass
+        result = list()
+
+        order = self.read(_id)
+        
+        order_attachments = OrderAttachment.select().where(OrderAttachment.order_id==order.id).get()
+
+        for order_attachment in order_attachments:
+
+            attachment = Attachment.select().where(Attachment.id==order_attachment.attachment_id).get()
+
+            result.append(attachment)
+
+        return result
