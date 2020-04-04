f06739dc3cf43a5f726efcc9165d9de49785d121
Nelson Carrasquel <carrasquel@outlook.com>
2020-04-04 12:00:25 -0300

    Order view in blueprint

--- app/blueprints/supervisor.py
+++ app/blueprints/supervisor.py
@@ -7,6 +7,7 @@ from flask_login import login_required, current_user
 from . import supervisor
 from .utils import role_required, get_credentials
 
+
 @supervisor.route('/supervisor')
 @login_required
 @role_required(["SUPERVISOR", "SUPERVISOR/MANAGER"])
@@ -15,3 +16,13 @@ def supervisor_page():
     credentials = get_credentials()
     
     return render_template("supervisor.html", credentials=credentials)
+
+
+@supervisor.route('/supervisor/orders/<int:order_id>')
+@login_required
+@role_required(["SUPERVISOR", "SUPERVISOR/MANAGER"])
+def supervisor_order_page(order_id):
+
+    credentials = get_credentials()
+    
+    return render_template("supervisor_order.html", order_id=order_id, credentials=credentials)
