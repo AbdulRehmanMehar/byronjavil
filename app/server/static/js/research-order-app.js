522eb6f35dd732bf5904ece31f57d0e51273ce00
Nelson Carrasquel <carrasquel@outlook.com>
2020-04-04 14:27:54 -0300

    Base app code for all roles

---
+++ app/server/static/js/research-order-app.js
@@ -0,0 +1,133 @@
+// order-detail-app.js
+
+var order_vm = new Vue({
+    el: '#app',
+
+    data: {
+        order: {},
+        apiKey: null,
+
+        comments: {},
+
+        edit: false
+    },
+
+    ready: function(){
+    },
+
+    methods: {
+
+        setKey: function(key){
+            this.apiKey = key;
+        },
+
+        closeModal: function(){
+            $('#user-create').modal('hide');
+        },
+
+        fetchOrder: function(){
+            var apiKey = this.apiKey;
+
+            this.$http.get('api/supervisor/users', {headers: {'X-API-KEY': apiKey}})
+                .then(function (res){
+                    this.users = res.data;
+                }, function(err){
+                    console.log(err);
+                })
+        },
+
+        fetchComments: function(){
+            var apiKey = this.apiKey;
+
+            this.$http.get('api/supervisor/users', {headers: {'X-API-KEY': apiKey}})
+                .then(function (res){
+                    this.users = res.data;
+                }, function(err){
+                    console.log(err);
+                })
+        },
+
+        fetchAttachment: function(){
+            var apiKey = this.apiKey;
+
+            this.$http.get('api/supervisor/users', {headers: {'X-API-KEY': apiKey}})
+                .then(function (res){
+                    this.users = res.data;
+                }, function(err){
+                    console.log(err);
+                })
+        },
+
+        postComment: function(){
+            
+            var apiKey = this.apiKey;
+
+            payload = this.$get("user");
+
+            waitingDialog.show('Sending');
+
+            if (this.edit){
+
+                var username = this.editUsername;
+                this.$http.put('/api/supervisor/users/' + username, payload, {headers: {'X-API-KEY': apiKey}})
+                    .then(function (res) {
+                        this.fetchUsers();
+                        this.resetUser()
+                        waitingDialog.hide();
+                    },
+                    function (err) {
+                        console.log(err);
+                });
+            }
+            else {
+                this.$http.post('/api/supervisor/users', payload, {headers: {'X-API-KEY': apiKey}})
+                    .then(function (res) {
+                        this.fetchUsers();
+                        this.resetUser()
+                        waitingDialog.hide();
+                    },
+                    function (err) {
+                        console.log(err);
+                });
+
+            }
+            
+        },
+
+        postAttachment: function(){
+            
+            var apiKey = this.apiKey;
+
+            payload = this.$get("user");
+
+            waitingDialog.show('Sending');
+
+            if (this.edit){
+
+                var username = this.editUsername;
+                this.$http.put('/api/supervisor/users/' + username, payload, {headers: {'X-API-KEY': apiKey}})
+                    .then(function (res) {
+                        this.fetchUsers();
+                        this.resetUser()
+                        waitingDialog.hide();
+                    },
+                    function (err) {
+                        console.log(err);
+                });
+            }
+            else {
+                this.$http.post('/api/supervisor/users', payload, {headers: {'X-API-KEY': apiKey}})
+                    .then(function (res) {
+                        this.fetchUsers();
+                        this.resetUser()
+                        waitingDialog.hide();
+                    },
+                    function (err) {
+                        console.log(err);
+                });
+
+            }
+            
+        }
+    }
+})
