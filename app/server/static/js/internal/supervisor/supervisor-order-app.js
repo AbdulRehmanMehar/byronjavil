25966a0f1df7f05d66aec5b8faf113e0f9e620e7
Nelson Carrasquel <carrasquel@outlook.com>
2020-05-12 17:43:01 -0400

    Map integrated into supervisor order

--- app/server/static/js/internal/supervisor/supervisor-order-app.js
+++ app/server/static/js/internal/supervisor/supervisor-order-app.js
@@ -26,6 +26,15 @@ var order_vm = new Vue({
 
     methods: {
         
+        initMap: function(){
+            var uluru = {lat: this.order.latitude, lng: this.order.longitude};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 10, center: uluru});
+            // The marker, positioned at Uluru
+            var marker = new google.maps.Marker({position: uluru, map: map});
+        },
+
         viewCompanyDetails: function(){
             var id = this.order.company.id;
             location.href = "/supervisor/companies/" + id;
