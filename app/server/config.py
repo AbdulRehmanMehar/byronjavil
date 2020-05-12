343a2e272a248ccc518e4a26150883d1c8bfe180
Nelson Carrasquel <carrasquel@outlook.com>
2020-05-12 23:15:16 +0000

    Server changes

--- app/server/config.py
+++ app/server/config.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-# instance/config.py
-
-SECRET_KEY = "42fee66a-08ff-4c2a-beab-ebc52ca4ada4"
-
-DATABASE = {
-    'user': 'postgres',
-    'password': 'prosperidad2020',
-    'host': '127.0.0.1',
-    'port': 5432
-}
-
-=======
 # instance/config.py
 
 SECRET_KEY = "42fee66a-08ff-4c2a-beab-ebc52ca4ada4"
@@ -22,5 +9,4 @@ DATABASE = {
     'port': 5432
 }
 
->>>>>>> fce5f45685d7399853f573c8ec7f8c1a2fe203f7
 DEBUG = False
