2c7ac80a6f22e5188b2a0ce64ed44dfd9905b077
Abdul Rehman <mehars.6925@gmail.com>
2020-09-29 03:33:30 +0500

    added routes to download csv and xlsx. Now handles XLSX upload as well!

--- app/api/order.py
+++ app/api/order.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # app/api/order.py
-
+import mimetypes
 from datetime import datetime
-
-from flask import request
+import csv, StringIO, xlsxwriter
+from openpyxl import load_workbook
+from flask import request, make_response, Response
 from flask_restplus import Resource, fields
 from playhouse.shortcuts import model_to_dict
 
 from app.server import server
-from app.models import OrderState, OrderType
+from app.models import OrderState, OrderType, ResearchType
 from app.email import send_mail_template
 
 from .utils import token_required, role_required, order_states
@@ -48,7 +49,11 @@ class SupervisorOrderCollection(Resource):
 
             response["assigned_date"] = str(response["assigned_date"])
             response["due_date"] = str(response["due_date"])
-            response["state"]["state"] = order_states[response["state"]["state"]]
+            if response["state"]:
+                response["state"]["state"] = order_states[response["state"]["state"]]
+            else:
+                response["state"] = {}
+                response["state"]["state"] = 'Not Assigned for Research!'
             
             result.append(response)
 
@@ -67,16 +72,23 @@ class SupervisorOrderCollection(Resource):
         type_dbo = app.order_type_dbo
         company_dbo = app.company_dbo
         client_dbo = app.client_code_dbo
-
+        use_research = payload["use_research"]
         research_id = payload["research_id"]
         data_id = payload["data_id"]
         company_id = payload["company_id"]
+        research_type_id = payload["research"]
 
         data_user = user_dbo.read_by_id(data_id)
         company = company_dbo.read(company_id)
         order_type = type_dbo.read_by_type(payload["type"])
         client_code = client_dbo.read_by_code(payload["client_code"])
-        
+
+        if research_type_id:
+            research_type = ResearchType.select().where(ResearchType.id==research_type_id).get()
+        else:
+            research_type = None
+
+
         if research_id:
             research_user = user_dbo.read_by_id(research_id)
             state = "RESEARCH"
@@ -85,6 +97,10 @@ class SupervisorOrderCollection(Resource):
             
             state = "DATA_ENTRY"
             research_user = None
+
+        if not use_research:
+            research_user = None
+            research_type = None
 
         order_state = OrderState.select().where(OrderState.state==state).get()
 
@@ -100,6 +116,7 @@ class SupervisorOrderCollection(Resource):
             "latitude": _lat,
             "longitude": _long,
             "data_user": data_user,
+            "research_type": research_type,
             "research_user": research_user,
             "company": company,
             "due_date": due_date,
@@ -143,7 +160,11 @@ class SupervisorOrder(Resource):
 
         response["assigned_date"] = str(response["assigned_date"])
         response["due_date"] = str(response["due_date"])
-        response["state"]["state"] = order_states[response["state"]["state"]]
+        if response['state']:
+            response["state"]["state"] = order_states[response["state"]["state"]]
+        else:
+            response["state"] = {}
+            response["state"]["state"] = 'Not Assigned for Research!'
 
         return response
 
@@ -160,27 +181,45 @@ class SupervisorOrder(Resource):
         type_dbo = app.order_type_dbo
         company_dbo = app.company_dbo
         client_dbo = app.client_code_dbo
-
+        use_research = payload["use_research"]
+        research_type_id = payload["research"]
         research_id = payload["research_id"]
         data_id = payload["data_id"]
         company_id = payload["company_id"]
 
-        research_user = user_dbo.read_by_id(research_id)
+        if research_type_id:
+            research_type = ResearchType.select().where(ResearchType.id==research_type_id).get()
+            # research_type =
+        else:
+            research_type = None
+
+        if research_id:
+            research_user = user_dbo.read_by_id(research_id)
+            state = "RESEARCH"
+            order_state = OrderState.select().where(OrderState.state==state).get()
+        else:
+            order_state = None
+            research_user = None
+
+        if not use_research:
+            order_state = None
+            research_user = None
+            research_type = None
+
         data_user = user_dbo.read_by_id(data_id)
         company = company_dbo.read(company_id)
         order_type = type_dbo.read_by_type(payload["type"])
         client_code = client_dbo.read_by_code(payload["client_code"])
-        state = "RESEARCH"
-        order_state = OrderState.select().where(OrderState.state==state).get()
 
         full_address = geo_address(payload["address"])
         _lat, _long = geo_coordinates(payload["address"])
-
+        print(research_type)
         data = {
             "address": payload["address"],
             "full_address": full_address,
             "latitude": _lat,
             "longitude": _long,
+            "research_type": research_type,
             "research_user": research_user,
             "data_user": data_user,
             "company": company,
@@ -194,7 +233,8 @@ class SupervisorOrder(Resource):
         dbo.update(_id, **data)
 
         order = dbo.read(_id)
-        send_mail_template("ASSIGN_RESEARCH", order, username=research_user.username, from_email="admin@pams.com", to_emails=[research_user.email])
+        if research_user:
+            send_mail_template("ASSIGN_RESEARCH", order, username=research_user.username, from_email="admin@pams.com", to_emails=[research_user.email])
 
         return True
 
@@ -206,5 +246,241 @@ class SupervisorOrder(Resource):
         dbo = app.order_dbo
 
         dbo.delete(_id)
+
+        return True
+
+
+@ns.route('/orders/get-csv')
+class SupervisorOrderCSV(Resource):
+    def get(self):
+        si = StringIO.StringIO()
+        dbo = app.order_dbo
+        orders = dbo.read_all()
+        head = [
+            "address",
+            "full_address",
+            "latitude",
+            "logintude",
+            "assigned_date",
+            "due_date",
+            "research_completed",
+            "data_completed",
+            "supervisor_picture",
+            "data_picture",
+            "manager_submit",
+            "company_id",
+            "research_user_id",
+            "data_user_id",
+            "client_code_id",
+            "kind_id",
+            "research_type_id",
+            "state_id"
+        ]
+        cw = csv.writer(si, dialect="excel")
+        cw.writerow(head)
+
+        for order in orders:
+            address = order.address
+            full_address = order.full_address,
+            latitude = order.latitude
+            lonitude = order.longitude
+            assgined_date = order.assigned_date
+            due_date = order.due_date
+            research_completed = order.research_completed
+            data_completed = order.data_completed
+            supervisor_picture = order.supervisor_picture
+            data_picture = order.data_picture
+            manager_submit = order.manager_submit
+            company_id = order.company
+            research_user_id = order.research_user
+            data_user_id = order.data_user
+            client_code_id = order.client_code
+            kind_id = order.kind
+            research_type_id = order.research_type
+            state_id = order.state
+            cw.writerow([address, full_address, latitude, lonitude, assgined_date, due_date, research_completed, data_completed, supervisor_picture, data_picture, manager_submit, company_id, research_user_id, data_user_id, client_code_id, kind_id, research_type_id, state_id])
+
+        output = make_response(si.getvalue())
+        output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        output.headers["Content-type"] = "text/csv"
+
+        return output
+
+
+def convert_tuple(value):
+    return str(value)
+
+
+@ns.route('/orders/get-xlsx')
+class SupervisorOrderXLSX(Resource):
+    def get(self):
+
+        dbo = app.order_dbo
+        orders = dbo.read_all()
+        response = Response()
+        response.status_code = 200
+        output = StringIO.StringIO()
+        workbook = xlsxwriter.Workbook(output)
+        worksheet = workbook.add_worksheet('orders')
+        bold = workbook.add_format({'bold': 1})
+
+        head = [
+            "address",
+            "full_address",
+            "latitude",
+            "logintude",
+            "assigned_date",
+            "due_date",
+            "research_completed",
+            "data_completed",
+            "supervisor_picture",
+            "data_picture",
+            "manager_submit",
+            "company_id",
+            "research_user_id",
+            "data_user_id",
+            "client_code_id",
+            "kind_id",
+            "research_type_id",
+            "state_id"
+        ]
+
+        worksheet.write_row('A1', head, bold)
+        idx = 0
+        for order in orders:
+            idx += 1
+            address = order.address
+            full_address = order.full_address,
+            latitude = order.latitude
+            lonitude = order.longitude
+            assgined_date = order.assigned_date
+            due_date = order.due_date
+            research_completed = order.research_completed
+            data_completed = order.data_completed
+            supervisor_picture = order.supervisor_picture
+            data_picture = order.data_picture
+            manager_submit = order.manager_submit
+            company_id = order.company
+            research_user_id = order.research_user
+            data_user_id = order.data_user
+            client_code_id = order.client_code
+            kind_id = order.kind
+            research_type_id = order.research_type
+            state_id = order.state
+
+            data = [address, full_address, latitude, lonitude, assgined_date, due_date, research_completed, data_completed, supervisor_picture, data_picture, manager_submit, company_id, research_user_id, data_user_id, client_code_id, kind_id, research_type_id, state_id]
+            data = map(convert_tuple, data)
+            worksheet.write_row(idx, 0, data)
+
+        workbook.close()
+        output = make_response(output.getvalue())
+        output.headers["Content-Disposition"] = "attachment; filename=export.xlsx"
+        output.headers["Content-type"] = "text/xlsx"
+
+        return output
+
+
+@ns.route('/orders/create/from-xlsx')
+class SupervisorCreateOrdersFromXLSX(Resource):
+    @api.doc(security='apikey')
+    @token_required
+    @role_required(["SUPERVISOR", "SUPERVISOR/MANAGER"])
+    def post(self):
+        dbo = app.order_dbo
+        user_dbo = app.user_dbo
+        type_dbo = app.order_type_dbo
+        company_dbo = app.company_dbo
+        client_dbo = app.client_code_dbo
+
+        file = request.files['xlsx']
+        workbook = load_workbook(file, read_only=True)
+
+        field_idxs = []
+        field_names = [
+            "address",
+            "assigned_date",
+            "due_date",
+            "company_id",
+            "research_user_id",
+            "data_user_id",
+            "client_code_id",
+            "kind_id",
+            "research_type_id",
+        ]
+
+        sheet_idx = -1
+        for sheet in workbook.sheetnames:
+            sheet_idx += 1
+
+            print sheet
+            row_idx = -1
+            for row in workbook[sheet]:
+                row_idx += 1
+                if row_idx == 0:
+                    col_idx = -1
+                    for col in row:
+                        col_idx += 1
+                        if col.value in field_names:
+                            field_idxs.append(col_idx)
+                else:
+                    order_data = []
+                    for idxs in field_idxs:
+                        order_data.append(row[idxs].value)
+
+                    if order_data:
+                        data_user = user_dbo.read_by_id(order_data[4])
+                        company = company_dbo.read(order_data[3])
+                        order_type = type_dbo.read(order_data[7])
+                        client_code = client_dbo.read(order_data[6])
+
+                        if order_data[8]:
+                            research_type = ResearchType.select().where(ResearchType.id==order_data[8]).get()
+                        else:
+                            research_type = None
+
+
+                        if order_data[6]:
+                            research_user = user_dbo.read_by_id(order_data[6])
+                            state = "RESEARCH"
+
+                        else:
+
+                            state = "DATA_ENTRY"
+                            research_user = None
+
+
+                        order_state = OrderState.select().where(OrderState.state==state).get()
+
+                        # due_date = datetime.strptime(str(order_data[2]), "%m/%d/%Y").date()
+                        # assigned_date = datetime.strptime(str(order_data[1]), "%m/%d/%Y").date()
+                        due_date = order_data[2].date()
+                        assigned_date = order_data[1].date()
+                        full_address = geo_address(order_data[0])
+                        _lat, _long = geo_coordinates(order_data[0])
+
+                        data = {
+                            "address": order_data[0],
+                            "full_address": full_address,
+                            "latitude": _lat,
+                            "longitude": _long,
+                            "data_user": data_user,
+                            "research_type": research_type,
+                            "research_user": research_user,
+                            "company": company,
+                            "due_date": due_date,
+                            "assigned_date": assigned_date,
+                            "client_code": client_code,
+                            "kind": order_type or None,
+                            "state": order_state
+                        }
+
+                        order = dbo.create(**data)
+
+                        order.save()
+
+                        if research_user:
+                            send_mail_template("ASSIGN_RESEARCH", order, username=research_user.username, from_email="admin@pams.com", to_emails=[research_user.email])
+                        else:
+                            send_mail_template("ASSIGN_DATA", order, username=data_user.username, from_email="admin@pams.com", to_emails=[data_user.email])
 
         return True
